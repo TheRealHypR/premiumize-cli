9f0068958d78d9b92279486fa2fba93556a17247
Peter <peter@gmail.com>
2017-12-30 14:47:34 +0100

    Fix grep not working with "-" in URLs and checking if URLs are available at premiumize

--- premiumize-cli.sh
+++ premiumize-cli.sh
@@ -6,7 +6,7 @@ pass="[USERPASS]"
 
 case "$1" in
 	*.dlc)
-		links=$(curl -s 'http://dcrypt.it/decrypt/paste' --data-urlencode "content@$1" |jq -r .success.links|grep -Po '[\w\d:/\.]+')
+		links=$(curl -s 'http://dcrypt.it/decrypt/paste' --data-urlencode "content@$1" |jq -r .success.links|grep -Po ' "\K[^"]*')
 		;;
 	*.links)
 		links=$(cat $1)
@@ -19,15 +19,21 @@ case "$1" in
 		echo "A File containing a list of URLs"
 		exit 1
 esac
-
-
 for i in $links;
 do
 	JSON=$(curl -s "https://api.premiumize.me/pm-api/v1.php?method=directdownloadlink&params\[login\]=$id&params\[pass\]=$pass&params\[link\]=$i")
 
-	LOCATION=$(echo $JSON | jq -r .result.location)
-	NAME=$(echo $JSON | jq -r .result.filename)
+	STATUS=$(echo $JSON | jq -r .status)
+	MESSAGE=$(echo $JSON | jq -r .statusmessage)
 
-	echo $NAME
-	curl --progress-bar -o $NAME $LOCATION
+	if [ $STATUS == 200 ] 
+	then
+		LOCATION=$(echo $JSON | jq -r .result.location)
+		NAME=$(echo $JSON | jq -r .result.filename)
+
+		echo $NAME
+		curl --progress-bar -o $NAME $LOCATION
+	else
+		echo "Error:" $MESSAGE
+	fi
 done
